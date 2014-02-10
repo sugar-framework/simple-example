7fd496241c9e477c762ac4bdb6c7973a56ebd2c6
Shane Logsdon <shane@shanelogsdon.com>
2014-02-09 22:14:44 -0500

    adding demo resource controller

---
+++ lib/simple/controllers/pages.ex
@@ -0,0 +1,43 @@
+defmodule Simple.Controllers.Pages do
+  use Sugar.Controller
+
+  # GET /pages
+  def index(conn, _args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # GET /pages/new
+  def new(conn, _args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # POST /pages
+  def create(conn, _args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # GET /pages/:id
+  def show(conn, args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # GET /pages/:id/edit
+  def edit(conn, args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # PUT /pages/:id
+  def update(conn, args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # PATCH /pages/:id
+  def patch(conn, args) do
+    halt! conn, [message: "Not implemented"]
+  end
+
+  # DELETE /pages/:id
+  def delete(conn, args) do
+    halt! conn, [message: "Not implemented"]
+  end
+end
